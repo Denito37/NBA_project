dfaab08c43e05599ce5f91d408c2da7a3982be51
Denito37 <69406178+Denito37@users.noreply.github.com>
2026-03-08 01:20:51 -0500

    Updated dashboard to keep track of FG_PCT changes

--- view/report.py
+++ view/report.py
@@ -1,12 +1,100 @@
 """
 Docstring for view.report
 """
+import sqlite3
 import streamlit as st
 import pandas as pd
-import seaborn as sns
+import numpy as np
 
-st.write('HELLOO My **WORLD**!')
+st.title('NBA Performace Report')
 
-# connect to Postgres database
+# connect to database
+conn = sqlite3.connect('NBA.db')
 
-# write lineplot into screen
+teams = pd.read_sql(
+    """
+    SELECT DISTINCT TEAM_NAME
+    FROM Team_data
+    WHERE SEASON_ID = 22025
+    """
+,conn)
+NBA_teams = list(teams.values)
+NBA_teams = sorted(NBA_teams)
+NBA_teams = np.array(NBA_teams).flatten().tolist()
+# Tabs
+tab1, tab2 = st.tabs(['Teams', 'Players'])
+
+# Team Tab
+with tab1:
+    choice = st.selectbox("Select Team",(NBA_teams))
+    # Query Database
+    team_df = pd.read_sql_query(f"""
+                                SELECT * 
+                                FROM Team_data
+                                WHERE TEAM_NAME = '{choice}'
+                                """
+                                ,conn)
+    team_win_count_L10 = pd.read_sql_query(f"""
+                                        SELECT COUNT(WL) AS TOTAL_WINS
+                                        FROM (SELECT * FROM Team_data WHERE TEAM_NAME = '{choice}' LIMIT 10) AS LAST_TEN
+                                        WHERE WL = "W" AND TEAM_NAME = '{choice}'
+                                        """
+                                        ,conn)
+    FG_PCT_AVG_L10 = pd.read_sql_query(f"""
+                                        SELECT AVG(FG_PCT) AS AVERAGE_FG
+                                        FROM (SELECT * FROM Team_data WHERE TEAM_NAME = '{choice}' LIMIT 10) AS LAST_TEN
+                                        WHERE TEAM_NAME = '{choice}'
+                                        """
+                                        ,conn)
+    FG_PCT_AVG = pd.read_sql_query(f"""
+                                    SELECT AVG(FG_PCT) AS AVERAGE
+                                    FROM Team_data
+                                    WHERE TEAM_NAME = '{choice}'
+                                    """
+                                    ,conn)
+    FG3_PCT_AVG_L10 = pd.read_sql_query(f"""
+                                        SELECT AVG(FG3_PCT) AS AVERAGE_FG
+                                        FROM (SELECT * FROM Team_data WHERE TEAM_NAME = '{choice}' LIMIT 10) AS LAST_TEN
+                                        WHERE TEAM_NAME = '{choice}'
+                                        """
+                                        ,conn)
+    FG3_PCT_AVG = pd.read_sql_query(f"""
+                                    SELECT AVG(FG3_PCT) AS AVERAGE
+                                    FROM Team_data
+                                    WHERE TEAM_NAME = '{choice}'
+                                    """
+                                    ,conn)
+    st.caption(f'{choice} Game data 2015 - present')
+
+    wins = team_win_count_L10.values[0][0]
+    losses = 10 - team_win_count_L10.values[0][0]
+    PCT_change = np.round((FG_PCT_AVG_L10.values - FG_PCT_AVG.values), decimals=3)
+    PCT3_change = np.round((FG3_PCT_AVG_L10.values - FG3_PCT_AVG.values), decimals=3)
+
+    metric1, metric2, metric3 = st.columns(3)
+    metric1.metric(
+        label= 'Last 10 Games',
+        value= f'{wins} W - {losses} L',
+        )
+    metric2.metric(
+        label='FG_PCT Last 10 Average',
+        value= np.round(FG_PCT_AVG_L10.values, decimals=3),
+        delta= float(PCT_change[0][0])
+    )
+    metric3.metric(
+        label='FG3_PCT Last 10 Average',
+        value= np.round(FG3_PCT_AVG_L10.values, decimals=3),
+        delta= float(PCT3_change[0][0])
+    )
+    st.scatter_chart(
+        team_df,
+        x = 'PTS',
+        y = 'FG_PCT',
+    )
+    st.caption('Data Table')
+    st.dataframe(team_df)
+
+# Player Tab
+with tab2:
+    choice = st.selectbox("Select Player", ('Jalen Brunson'))
+    st.caption(f"{choice}'s player data 2015 - Present ")
